223df4226ee7db2965bec644eedbe7f2736ae624
SeyedHossein <s.m.h.husseini@gmail.com>
2022-04-02 19:20:18 +0430

    First Commit

---
+++ src/Views/SignupMenu.java
@@ -0,0 +1,15 @@
+package Views;
+
+import java.util.Scanner;
+
+public class SignupMenu {
+    private Scanner scanner;
+
+    public SignupMenu(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public void run(){
+        
+    }
+}
